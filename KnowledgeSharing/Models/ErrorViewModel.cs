a3045e44541650dfb2d7579c699429deac8719df
Kirill <128813627+SkKirill@users.noreply.github.com>
2024-11-17 14:44:44 +0300

    Add project files.

---
+++ KnowledgeSharing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KnowledgeSharing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
