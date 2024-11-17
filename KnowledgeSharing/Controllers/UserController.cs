fb160968a36872d2beca51b84d5ce87d84b3d98f
Kirill <128813627+SkKirill@users.noreply.github.com>
2024-11-17 17:15:44 +0300

    New Pages

---
+++ KnowledgeSharing/Controllers/UserController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnowledgeSharing.Controllers
+{
+	public class UserController : Controller
+	{
+		// GET: UserController
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: UserController/Details/5
+		public IActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: UserController/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: UserController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: UserController/Edit/5
+		public IActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: UserController/Delete/5
+		public IActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
